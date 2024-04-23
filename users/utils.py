e34e00a74d0b305dc3e042a87551174832c5d494
Yuliia Shaposhnikova <domik339@gmail.com>
2024-04-23 15:37:33 +0300

    Fixed mistakes in utils.py

--- users/utils.py
+++ users/utils.py
@@ -1,12 +1,10 @@
-from django.core.mail import send_mail
 import os
-from django.core.mail import EmailMessage
+
+from django.core.mail import send_mail
 
 
 class Util:
     @staticmethod
     def send_email(data):
-        send_mail(subject=data['email_subject'], message=data['email_body'], from_email=os.environ.get('EMAIL_HOST_USER'), recipient_list=[data['to_email']])
-        email = EmailMessage(subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-        email.send()
-
+        send_mail(subject=data['email_subject'], message=data['email_body'],
+                  from_email=os.environ.get('EMAIL_HOST_USER'), recipient_list=[data['to_email']])
