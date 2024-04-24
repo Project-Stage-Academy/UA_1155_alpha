67f59bed6bdf474d5d02e12f53cf0ce7d5ddcb78
Mariia Novytska <mnovickaa@gmail.com>
2024-04-24 17:33:09 +0300

    Added django_rest_passwordreset==1.4.1 to the requitements.txt, checked user creation before PR

--- users/urls.py
+++ users/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import UserRegisterAPIView, LoginAPIView, LogoutAPIView, SendEmailConfirmationAPIView
+from .views import UserRegisterAPIView, LoginAPIView, LogoutAPIView, VerifyEmailAPIView
 
 
 urlpatterns = [
     path('register/',  UserRegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
-    path('verify-email/<str:token>/<int:user_id>/', SendEmailConfirmationAPIView.as_view(), name='verify-email')
+    path('verify-email/<str:token>/<int:user_id>/', VerifyEmailAPIView.as_view(), name='verify-email')
 ]
 
 
