8c0c1639c23a54f45903dc2a02aaf07d4565de01
Alex <alexandersychev2005@gmail.com>
2024-04-25 16:41:08 +0300

    Changed models with null and blank fields

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db.models.functions import Now
 from django.contrib.auth.hashers import make_password
 from uuid import uuid4
+from startups.models import Project
 
 
 class CustomUserManager(BaseUserManager):
@@ -74,11 +75,11 @@ class CustomUser(AbstractBaseUser):
 class Investor(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='investors')
-    location = models.CharField(max_length=128)
-    contact_phone = models.CharField(max_length=25)
+    location = models.CharField(max_length=128, blank=True)
+    contact_phone = models.CharField(max_length=25, blank=True)
     contact_email = models.EmailField(unique=True, max_length=50)
     investment_amount = models.DecimalField(max_digits=12, decimal_places=2)
-    interests = models.CharField(max_length=500)
+    interests = models.CharField(max_length=500, blank=True, choices=Project.INDUSTRY_CHOICES)
     number_for_investor_validation = models.IntegerField(null=True)
 
     class Meta:
