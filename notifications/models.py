8a3fac5e75d695dedc1c0b979a098695d547cf06
Alex <alexandersychev2005@gmail.com>
2024-05-01 16:56:41 +0300

    Added notifications model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import mongoengine
 
-# Create your models here.
+from datetime import datetime
+
+
+class Notification(mongoengine.Document):
+    TYPES_OF_NOTIFICATIONS = [
+        ("project_updating", "Project Updating"),
+    ]
+
+    investor_id = mongoengine.IntField(required=True)
+    project_id = mongoengine.IntField(required=True)
+    type_of_notification = mongoengine.StringField(choices=TYPES_OF_NOTIFICATIONS)
+    send_at = mongoengine.DateTimeField(default=datetime.now)
+    is_read = mongoengine.BooleanField(default=False)
+    text = mongoengine.StringField(required=True, max_length=1000)
