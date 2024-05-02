08a65fa53f67dd3c4603ae2200d589a512e61c87
Alex <alexandersychev2005@gmail.com>
2024-05-02 23:48:41 +0300

    Added send email for project updating for project subscribers

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -1,15 +1,28 @@
-import os
 from celery import shared_task
-from django.core.mail import send_mail
+
+from investors.models import Investor
+from projects.models import Project
+from users.models import CustomUser
 from .models import Notification
-
-
-def send_email_util(email_subject, email_body, to_email):
-    send_mail(subject=email_subject, message=email_body,
-              from_email=os.environ.get('EMAIL_HOST_USER'), recipient_list=[to_email])
+from .utils import Util
 
 
 @shared_task(bind=True)
 def project_updating(self, investor_id, project_id):
-    # Notification(investor_id=investor_id, project_id=project.id, )
-    pass
+    project = Project.objects.get(id=project_id)
+    investor = Investor.objects.get(id=investor_id)
+    user = CustomUser.objects.get(id=investor.user_id)
+    notification = Notification(
+        investor_id=investor_id,
+        project_id=project_id,
+        type_of_notification='project_updating',
+        text=f"Project {project.project_name} has been updated"
+    )
+    notification.save()
+
+    sent_data = {
+        "email_subject": "Project Updated",
+        "email_body": f"Hello {user.first_name}!\nThe project {project.project_name} has been updated.",
+        "to_email": investor.contact_email
+    }
+    Util.send_email(sent_data)
