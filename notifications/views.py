b506e1489e7fd4509a7008c1080d87ff02bdd9ae
Alex <alexandersychev2005@gmail.com>
2024-05-07 14:31:59 +0300

    Fixed notifications view

--- notifications/views.py
+++ notifications/views.py
@@ -4,7 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from notifications.models import Notification
 from .serializers import NotificationSerializer
-
+from investors.models import Investor
+from startups.models import Startup
 
 # Create your views here.
 class NotificationListView(APIView):
@@ -15,12 +16,14 @@ class NotificationListView(APIView):
         user = request.user
 
         if user.is_investor:
-            investor_notifications = Notification.objects.filter(recipient_id=user.id, recipient_type="investor",
+            investor_id = Investor.objects.get(user_id=user.id).id
+            investor_notifications = Notification.objects.filter(recipient_id=investor_id, recipient_type="investor",
                                                                  is_read=False)
             notifications.extend(investor_notifications)
             investor_notifications.update(is_read=True)
         if user.is_startup:
-            startup_notifications = Notification.objects.filter(recipient_id=user.id, recipient_type="startup",
+            startup_id = Startup.objects.get(owner_id=user.id).id
+            startup_notifications = Notification.objects.filter(recipient_id=startup_id, recipient_type="startup",
                                                                 is_read=False)
             notifications.extend(startup_notifications)
             startup_notifications.update(is_read=True)
