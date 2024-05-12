6787d0f779ef946b52dae0bcadd35893d910a810
Alex <alexandersychev2005@gmail.com>
2024-05-12 14:35:30 +0300

    Added celery task for project subscription

--- investors/views.py
+++ investors/views.py
@@ -4,11 +4,8 @@ from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-<<<<<<< HEAD
-=======
-
 from users.models import CustomUser
->>>>>>> main
+
 
 from projects.models import Project
 from projects.serializers import ProjectSerializer
--- notifications/views.py
+++ notifications/views.py
@@ -1,16 +1,17 @@
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from notifications.models import Notification
 from .serializers import NotificationSerializer
+from rest_framework.decorators import action
 
 
-# Create your views here.
-class NotificationListView(APIView):
+class NotificationsViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    @action(detail=False, methods=['get'], url_path='new')
+    def get_my_new_notifications(self, request):
         user = request.user
 
         notifications = Notification.get_unread_notifications(recipient_id=user.id, is_read=False)
@@ -24,11 +25,8 @@ class NotificationListView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-class NotificationsAllView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
+    @action(detail=False, methods=['get'], url_path='all')
+    def get_my_all_notifications(self, request):
         user = request.user
 
         notifications = Notification.get_all_notifications(recipient_id=user.id)
