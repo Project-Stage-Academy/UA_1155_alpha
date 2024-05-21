6a8916aee35f6783a59e045031ed0f543dddd245
superolegatron <olegtsirkun@gmail.com>
2024-05-21 14:52:36 +0300

    Separated notification classes and added logging content moderation history to mongodb

--- notifications/models.py
+++ notifications/models.py
@@ -1,26 +1,39 @@
 import mongoengine
-
 from datetime import datetime
 
 
 class Notification(mongoengine.Document):
-    TYPES_OF_NOTIFICATIONS = [
-        ("project_updating", "Project Updating"),
-        ("investor_subscription", "Investors Subscription"),
-
-    ]
     TYPES_OF_RECIPIENTS = [
         ("investor", "Investor"),
-        ("startup", "Startup")
+        ("startup", "Startup"),
+        ("project", "Project")
     ]
 
     recipient_id = mongoengine.IntField(required=True)
     recipient_type = mongoengine.StringField(choices=TYPES_OF_RECIPIENTS)
+    send_at = mongoengine.DateTimeField(default=datetime.now)
+    text = mongoengine.StringField(required=True, max_length=1000)
+
+    @classmethod
+    def create_notification(cls, recipient_type, recipient_id, text):
+        notification = cls(
+            recipient_type=recipient_type,
+            recipient_id=recipient_id,
+            text=text
+        )
+        notification.save()
+        return notification
+
+
+class ProjectNotification(Notification):
+    TYPES_OF_NOTIFICATIONS = [
+        ("project_updating", "Project Updating"),
+        ("investor_subscription", "Investors Subscription"),
+    ]
+
     project_id = mongoengine.IntField(required=True)
     type_of_notification = mongoengine.StringField(choices=TYPES_OF_NOTIFICATIONS)
-    send_at = mongoengine.DateTimeField(default=datetime.now)
     is_read = mongoengine.BooleanField(default=False)
-    text = mongoengine.StringField(required=True, max_length=1000)
 
     @classmethod
     def create_notification(cls, recipient_type, recipient_id, project_id, type_of_notification, text):
@@ -35,11 +48,12 @@ class Notification(mongoengine.Document):
         return notification
 
     @staticmethod
-    def get_unread_notifications(recipient_id, is_read):
-        notifications = Notification.objects.filter(recipient_id=recipient_id, is_read=is_read).order_by('-send_at')
+    def get_unread_notifications(recipient_id, is_read=False):
+        notifications = ProjectNotification.objects.filter(recipient_id=recipient_id, is_read=is_read).order_by(
+            '-send_at')
         return notifications
 
     @staticmethod
     def get_all_notifications(recipient_id):
-        notifications = Notification.objects.filter(recipient_id=recipient_id).order_by('-send_at')
+        notifications = ProjectNotification.objects.filter(recipient_id=recipient_id).order_by('-send_at')
         return notifications
