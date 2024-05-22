12fd3247b575c7a361fab9c4ad13e8f8a0c198d1
Mariia Novytska <mnovickaa@gmail.com>
2024-05-22 12:31:40 +0300

    Created model and json for locations

--- investors/models.py
+++ investors/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
 from projects.models import Project
 from users.models import CustomUser
 
 
 class Investor(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='investors')
-    location = models.CharField(max_length=128, blank=True)
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="investors"
+    )
     contact_phone = models.CharField(max_length=25, blank=True)
     contact_email = models.EmailField(unique=True, max_length=50)
     investment_amount = models.DecimalField(max_digits=12, decimal_places=2)
@@ -17,9 +17,9 @@ class Investor(models.Model):
     is_verified = models.BooleanField(default=False)
 
     class Meta:
-        db_table = 'investors'
-        verbose_name = 'Investor'
-        verbose_name_plural = 'Investors'
+        db_table = "investors"
+        verbose_name = "Investor"
+        verbose_name_plural = "Investors"
 
     def __str__(self):
         return f"Investor: {self.user.first_name}. E-mail: {self.contact_email}"
--- projects/models.py
+++ projects/models.py
@@ -1,35 +1,66 @@
 from django.db import models
 from django.db.models.functions import Now
-from startups.models import Startup, Industry
+from startups.models import Industry, Startup
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "locations"
 
 
 class Project(models.Model):
     STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('approved', 'Approved'),
-        ('rejected', 'Rejected'),
-        ('in_progress', 'In Progress'),
-        ('completed', 'Completed'),
+        ("pending", "Pending"),
+        ("approved", "Approved"),
+        ("rejected", "Rejected"),
+        ("in_progress", "In Progress"),
+        ("completed", "Completed"),
     ]
 
-    startup = models.ForeignKey(Startup, on_delete=models.CASCADE, related_name='projects')
+    startup = models.ForeignKey(
+        Startup, on_delete=models.CASCADE, related_name="projects"
+    )
     project_name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
     goals = models.CharField(max_length=255)
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name="projects",
+        null=True,
+        blank=True,
+    )
     budget_needed = models.DecimalField(max_digits=10, decimal_places=2)
-    budget_ready = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
-    industry = models.ForeignKey(Industry, on_delete=models.CASCADE, related_name='projects', null=True, blank=True)
+    budget_ready = models.DecimalField(
+        max_digits=12, decimal_places=2, null=True, blank=True
+    )
+    industry = models.ForeignKey(
+        Industry,
+        on_delete=models.CASCADE,
+        related_name="projects",
+        null=True,
+        blank=True,
+    )
     promo_photo_url = models.CharField(max_length=1000, blank=True)
     promo_video_url = models.CharField(max_length=1000, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     registration_date = models.DateTimeField(default=Now())
     updated_at = models.DateTimeField(auto_now=True)
     rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    subscribers = models.ManyToManyField("investors.Investor", related_name='subscribed_projects', blank=True)
-    investors = models.ManyToManyField("investors.Investor", related_name='participated_projects', blank=True)
+    subscribers = models.ManyToManyField(
+        "investors.Investor", related_name="subscribed_projects", blank=True
+    )
+    investors = models.ManyToManyField(
+        "investors.Investor", related_name="participated_projects", blank=True
+    )
     is_active = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
 
     class Meta:
-        db_table = 'projects'
-        app_label = 'projects'
+        db_table = "projects"
+        app_label = "projects"
