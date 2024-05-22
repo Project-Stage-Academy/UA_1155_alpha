91b7153920d12dcd40344271a74fd3c642a69ce8
Mariia Novytska <mnovickaa@gmail.com>
2024-05-22 17:00:32 +0300

    Simple updates

--- forum/utils.py
+++ forum/utils.py
@@ -1,4 +1,5 @@
 import re
+
 from rest_framework import serializers
 
 
@@ -7,17 +8,20 @@ class ValidationPatterns:
     Class for validations in serializers
     """
 
-    PASSWORD_REGEX = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$'
-    PHONE_NUMBER_REGEX = r'^\+[0-9]{1,3}[0-9]{9}$'
-    LOCATION_REGEX = r'^([A-Za-z]+[\s,\-]?)+[A-Za-z]+$'
-    EDRPOU_REGEX = r'^\d{8}$'
-    FOP_REGEX = r'^\d{10}|\d{12}$'
+    PASSWORD_REGEX = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+    PHONE_NUMBER_REGEX = r"^\+[0-9]{1,3}[0-9]{9}$"
+    LOCATION_REGEX = r"^([A-Za-z]+[\s,\-]?)+[A-Za-z]+$"
+    EDRPOU_REGEX = r"^\d{8}$"
+    FOP_REGEX = r"^\d{10}|\d{12}$"
 
     @staticmethod
     def validate_password(password):
         if not re.match(ValidationPatterns.PASSWORD_REGEX, password):
-            raise serializers.ValidationError({
-                'Error': 'Password must contain at least 8 characters, 1 letter, 1 number and 1 special character'})
+            raise serializers.ValidationError(
+                {
+                    "Error": "Password must contain at least 8 characters, 1 letter, 1 number and 1 special character"
+                }
+            )
 
     @staticmethod
     def validate_passwords_match(password, password2):
@@ -32,20 +36,29 @@ class ValidationPatterns:
     @staticmethod
     def validate_location(location):
         if not re.match(ValidationPatterns.LOCATION_REGEX, location):
-            raise serializers.ValidationError({
-                "Error": "Location must be in the format 'Name Region' or 'Name, Region' and in English"})
+            raise serializers.ValidationError(
+                {
+                    "Error": "Location must be in the format 'Name Region' or 'Name, Region' and in English"
+                }
+            )
         if not location[0].isupper():
-            raise serializers.ValidationError({"Error": "Region name must start with an uppercase letter"})
+            raise serializers.ValidationError(
+                {"Error": "Region name must start with an uppercase letter"}
+            )
 
     @staticmethod
     def validate_edrpou(edrpou):
         if not re.match(ValidationPatterns.EDRPOU_REGEX, str(edrpou)):
-            raise serializers.ValidationError({"Error": "EDRPOU code must contain exactly 8 digits"})
+            raise serializers.ValidationError(
+                {"Error": "EDRPOU code must contain exactly 8 digits"}
+            )
 
     @staticmethod
     def validate_fop(fop):
         if not re.match(ValidationPatterns.FOP_REGEX, str(fop)):
-            raise serializers.ValidationError({'Error': 'FOP code must contain 10 or 12 digits'})
+            raise serializers.ValidationError(
+                {"Error": "FOP code must contain 10 or 12 digits"}
+            )
 
 
 def get_query_dict(request):
