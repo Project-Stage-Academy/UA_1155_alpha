e83322fb60136de0fe26ccf38838128935775cab
Yuliia Shaposhnikova <domik339@gmail.com>
2024-05-23 22:31:59 +0300

    Added new methods for signals and new settings for celery

--- forum/celery.py
+++ forum/celery.py
@@ -1,6 +1,6 @@
 import os
-from celery import Celery
-
+from celery import Celery, shared_task
+from django.core.mail import send_mail
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forum.settings')
 app = Celery('forum')
@@ -8,3 +8,8 @@ app = Celery('forum')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+
+@shared_task
+def send_email_notification(subject, message, recipient_list):
+    send_mail(subject, message, 'postmaster@sandboxfe2a54ed4bab462789981abc4acfa340.mailgun.org', recipient_list)
--- notifications/signals.py
+++ notifications/signals.py
@@ -6,7 +6,9 @@ from django.dispatch import receiver, Signal
 from investors.models import Investor
 from projects.models import Project
 from startups.models import Startup
+from .models import Notification
 from .tasks import send_for_moderation, project_updating, project_subscription
+from forum.celery import send_email_notification
 
 project_updated_signal = Signal()
 project_subscription_signal = Signal()
@@ -44,3 +46,23 @@ def project_subscription_receiver(sender, project_id, subscriber_id, **kwargs):
     request = get_current_request()
     current_site = get_current_site(request).domain if request else "localhost:8000/"
     project_subscription.delay(project_id, subscriber_id, current_site)
+
+
+def match_project_to_investor(project, investor):
+    project_industries = set(project.industries.values_list('id', flat=True))
+    investor_interests = set(investor.interests.values_list('id', flat=True))
+    return bool(project_industries & investor_interests)
+
+
+def send_notification(investor, project, is_new):
+    subject = "New Project" if is_new else "Project Update"
+    message = f"The project '{project.name}' in your area of interest has been {'created' if is_new else 'updated'}."
+    notification = Notification.objects.create(recipient=investor.user, message=message)
+    send_email_notification.delay(subject, message, [investor.contact_email])
+
+
+def notify_investors_about_project(project, is_new):
+    investors = Investor.objects.filter(is_active=True, is_verified=True)
+    for investor in investors:
+        if match_project_to_investor(project, investor):
+            send_notification(investor, project, is_new)
